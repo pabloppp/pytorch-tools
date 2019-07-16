3802a36af2afbf9bde98f17cf9e79f8f15244060
pablo pernias <pablo.pernias@frontiersin.org>
2019-07-16 14:39:50 +0200

    fix (embarassing) typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='toch_tools',
+    name='torchtools',
     packages=find_packages(),
     description='PyTorch useful tools',
     version='0.1.1',
