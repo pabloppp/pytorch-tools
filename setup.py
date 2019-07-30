351ef67dd148db1d7a9fcfe27a03e22b7fb5948d
pablo pernias <pablo.pernias@frontiersin.org>
2019-07-30 12:22:24 +0200

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='torchtools',
     packages=find_packages(),
     description='PyTorch useful tools',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/pabloppp/pytorch-tools',
     author='Pablo Pernías',
     author_email='pablo@pernias.com',
