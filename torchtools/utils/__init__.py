62c2ee704d78f83e7bd69acd3cfffbecd69ca970
pablo pernías <pablo@pernias.com>
2023-11-06 13:12:28 +0100

    rename remove_gamma_parametrizations

--- torchtools/utils/__init__.py
+++ torchtools/utils/__init__.py
@@ -1,3 +1,3 @@
 from .diffusion import Diffuzz
 from .diffusion2 import Diffuzz2
-from .gama_parametrization import apply_gamma_reparam, gamma_reparam_model, remove_parametrizations
+from .gama_parametrization import apply_gamma_reparam, gamma_reparam_model, remove_gamma_parametrizations
