a02f66ed7cfc89c1c32c282f5238367b41470ce8
pablo pernías <pablo@pernias.com>
2024-01-10 19:12:59 +0100

    set init_w for Gain

--- torchtools/nn/magnitude_preserving.py
+++ torchtools/nn/magnitude_preserving.py
@@ -10,9 +10,9 @@ class MP_SiLU(nn.SiLU):
         return super().forward(x) / 0.596 # ¯\_(ツ)_/¯
     
 class Gain(nn.Module):
-    def __init__(self):
+    def __init__(self, init_w=0.0):
         super().__init__()
-        self.g = nn.Parameter(torch.tensor([0.0]))
+        self.g = nn.Parameter(torch.tensor([init_w]))
 
     def forward(self, x):
         return x * self.g
